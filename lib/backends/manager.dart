5374b297b16f8cca111d6817ea533439af035196
nennneko_5787 <nennneko5787@gmail.com>
2024-06-03 10:39:43 +0000

    asdfghj

--- lib/backends/manager.dart
+++ lib/backends/manager.dart
@@ -186,7 +186,7 @@ class Manager {
       }
 
       String? replyed_to = null;
-      String? replyHTML = circle.querySelector('.reply-to').innerHTML;
+      String? replyHTML = circle.querySelector('.reply-to').text;
       if (replyHTML != null) {
         // 正規表現でマッチ
         RegExp regExp = RegExp(r'\/field\/(.*?)\/');
