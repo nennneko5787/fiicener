20151778d56a3240269f32e5192da26484192f0f
nennneko_5787 <nennneko5787@gmail.com>
2024-06-04 10:35:43 +0000

    aaaaaaa

--- lib/gui/circle.dart
+++ lib/gui/circle.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../backends/user.dart';
 import '../backends/circle.dart'; // Circle クラスを提供するファイルをインポート
 import '../backends/textagent.dart';
 import 'profile.dart';
@@ -118,41 +119,96 @@ class CircleDetailPage extends StatelessWidget {
         title: Text('サークル'),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                _buildCircleAvatar(context, circle),
-                const SizedBox(width: 8),
-                _buildUserInfo(circle),
-              ],
-            ),
-            const SizedBox(height: 8),
-            Text.rich(TextAgent.generate(circle.content)),
-            circle.imageUrl != null
-                ? FittedBox(
-                    child: Image.network('${circle.imageUrl}'),
-                    fit: BoxFit.contain,
-                  )
-                : SizedBox(),
-            circle.videoPoster != null
-                ? FittedBox(
-                    child: Image.network('${circle.videoPoster}'),
-                    fit: BoxFit.contain,
-                  )
-                : SizedBox(),
-            _buildActions(circle),
-            const Divider(
-              color: Colors.grey,
-              thickness: 1,
-              height: 2,
-            ),
-          ],
+      body: Row(children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  _buildCircleAvatar(context, circle),
+                  const SizedBox(width: 8),
+                  _buildUserInfo(circle),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Text.rich(TextAgent.generate(circle.content)),
+              circle.imageUrl != null
+                  ? FittedBox(
+                      child: Image.network('${circle.imageUrl}'),
+                      fit: BoxFit.contain,
+                    )
+                  : SizedBox(),
+              circle.videoPoster != null
+                  ? FittedBox(
+                      child: Image.network('${circle.videoPoster}'),
+                      fit: BoxFit.contain,
+                    )
+                  : SizedBox(),
+              _buildActions(circle),
+              const Divider(
+                color: Colors.grey,
+                thickness: 1,
+                height: 2,
+              ),
+            ],
+          ),
         ),
-      ),
+        FutureBuilder<List<Circle>>(
+          future: circle.getReplys(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return ListView(); // Empty ListView to show refresh indicator
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(
+                child: Text('No circles available'),
+              );
+            } else {
+              final circles = snapshot.data!;
+              return Scrollbar(
+                child: ListView.builder(
+                  itemCount: circles.length,
+                  itemBuilder: (context, index) {
+                    final c = circles[index];
+                    return ListTile(
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              _buildCircleAvatar(context, c),
+                              const SizedBox(width: 8),
+                              _buildUserInfo(c),
+                            ],
+                          ),
+                          const SizedBox(height: 8),
+                          Text.rich(TextAgent.generate(circle.content)),
+                          _buildActions(c),
+                          const Divider(
+                            color: Colors.grey,
+                            thickness: 1,
+                            height: 2,
+                          ),
+                        ],
+                      ),
+                      onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CircleDetailPage(circle: c)),
+                      ),
+                    );
+                  },
+                ),
+              );
+            }
+          },
+        ),
+      ]),
     );
   }
 }
