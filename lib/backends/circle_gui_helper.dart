f5ed76f90c50311a15b139a63c056296fde476a5
ねんねこ㌨ <nennneko5787@gmail.com>
2024-06-06 22:42:03 +0900

    circle_gui_helper.dart を更新

--- lib/backends/circle_gui_helper.dart
+++ lib/backends/circle_gui_helper.dart
@@ -20,28 +20,35 @@ class CircleGuiHelper {
                   child: InteractiveViewer(
                     minScale: 0.1,
                     maxScale: 5,
-                    child: Image.network(
-                      '${image}',
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height,
+                      child: FittedBox(
+                        child: Image.network(
+                          image ?? '',
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ],
             ),
-            Row(
-              children: [
-                Material(
-                  color: Colors.transparent,
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    icon: const Icon(
-                      Icons.close,
-                      size: 30,
-                    ),
+            Positioned(
+              top: 20.0,
+              right: 20.0,
+              child: Material(
+                color: Colors.transparent,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(
+                    Icons.close,
+                    size: 30,
+                    color: Colors.white,
                   ),
                 ),
-              ],
+              ),
             ),
           ],
         );
