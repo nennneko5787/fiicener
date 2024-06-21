3837d2582224948ff191491e6d76435c31caf8ce
RasPython3 <87003300+RasPython3@users.noreply.github.com>
2024-06-21 19:51:45 +0900

    Add modification to `network.dart`

--- lib/backends/network.dart
+++ lib/backends/network.dart
@@ -2,4 +2,68 @@ import "manager.dart";
 import "package:http/http.dart" as http;
 
 class HttpWrapper {
+  static Future<http.Response> get({
+    required Uri uri,
+    Map<String, String> headers
+  }) async {
+    String? session = await Manager.loadSessionToken();
+    String? csrf = await Manager.loadCsrfToken();
+
+    Map allheaders = {};
+    allheaders.addAll(headers)
+
+    if (allheaders.containsKey('Cookie')) {
+      allheaders['Cookie'] = allheaders['Cookie'] + 'sessionid=$session; csrftoken=$csrf;';
+    } else {
+      allheaders['Cookie'] = 'sessionid=$session; csrftoken=$csrf;';
+    }
+
+    final response = await http.get(
+      uri,
+      allheaders
+    );
+    return response;
+  }
+
+  static Future<http.Response> put({
+    required Uri uri,
+    Map<String, String> headers
+    Object body,
+    Encoding encoding = utf8
+  }) async {
+    String? session = await Manager.loadSessionToken();
+    String? csrf = await Manager.loadCsrfToken();
+
+    Map allheaders = {};
+    allheaders.addAll(headers)
+
+    if (allheaders.containsKey('Cookie')) {
+      allheaders['Cookie'] += 'sessionid=$session; csrftoken=$csrf';
+    } else {
+      allheaders['Cookie'] = 'sessionid=$session; csrftoken=$csrf';
+    }
+
+    if (!allheaders.containsKey('X-CSRFToken')) {
+      allheaders['X-CSRFToken'] = csrf;
+    }
+
+    dynamic data = body;
+
+    if (allheaders['Content-Type'] == 'multipart/form-data' && body is Map) {
+      final boundary = '--UNOFFICIAL-FIICENER';
+      allheaders['Content-Type'] += '; boundary=$boundary';
+      data = '';
+      for (String key in body.keys) {
+        data += '$boundary\r\nContent-Disposition: form-data; name="$key"\r\n\r\n${body[key]}\r\n';
+      }
+    }
+
+    final response = await http.get(
+      uri,
+      allheaders,
+      data,
+      encoding
+    );
+    return response;
+  }
 }
